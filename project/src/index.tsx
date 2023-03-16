eab320d78d55bc7f2f70e50f5dcc04e6a1402f87
Roman Katrovsky <katrovsky@gmail.com>
2023-03-16 17:00:15 +0300

    p2 подключает моки в индекс

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-
-const Setting = {
-  RentalOffersNumber: 5,
-} as const;
+import { offers } from './mocks/offers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -12,6 +9,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App rentalOffersNumber={Setting.RentalOffersNumber}/>
+    <App
+      rentalOffersNumber={offers.length}
+    />
   </React.StrictMode>,
 );
