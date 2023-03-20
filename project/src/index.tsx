c3452345aa62d112429fc5fec80323d212d2b501
Roman Katrovsky <katrovsky@gmail.com>
2023-03-20 08:49:56 +0300

    p3 обновляет карточку предложения

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { offers } from './mocks/offers';
+import { reviews } from './mocks/reviews';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -11,6 +12,8 @@ root.render(
   <React.StrictMode>
     <App
       rentalOffersNumber={offers.length}
+      offers={offers}
+      reviews={reviews}
     />
   </React.StrictMode>,
 );
--- project/src/mocks/reviews.ts
+++ project/src/mocks/reviews.ts
@@ -5,7 +5,7 @@ export const reviews: Review[] = [
     comment: 'Nice place.',
     date: 'Sun Mar 24 2022 13:13:13 GMT+0300 (Москва, стандартное время)',
     id: 1,
-    rating: 4,
+    rating: 2,
     user: {
       avatarUrl: 'img/avatar-max.jpg',
       id: 1,
@@ -15,9 +15,9 @@ export const reviews: Review[] = [
   },
   {
     comment: 'Not bad place.',
-    date: 'Tue Mar 29 2022 22:26:12 GMT+0300 (Москва, стандартное время)',
+    date: 'Tue Feb 28 2022 22:26:12 GMT+0300 (Москва, стандартное время)',
     id: 2,
-    rating: 5,
+    rating: 4,
     user: {
       avatarUrl: 'img/avatar-ben.jpg',
       id: 2,
