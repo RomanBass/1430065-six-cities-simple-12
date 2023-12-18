6e4fd803c8922e8002cea073ed4cf6bd3561c808
Roman Katrovsky Huawei Laptop <katrovsky@gmail.com>
2023-12-18 23:34:51 +0300

    завершает м2т1

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Property from '../../pages/main/property/property';
+
+type AppScreenProps = {
+  placesNumber: number;
+}
+
+function App({placesNumber}: AppScreenProps): JSX.Element {
+  return (
+    <Property/>
+  );
 }
 
 export default App;
